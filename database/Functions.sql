757fbfffdd0af7866f36b460a6948b2aff96c906
planetixin <gax36003@student.gdynia.merito.pl>
2026-04-12 21:07:51 +0200

    logowanie-database update1
    przeniesienie database.db do folderu database. Dodanie plików CreateTable.sql, Functions.sql

---
+++ database/Functions.sql
@@ -0,0 +1,39 @@
+
+
+SET @seed = "fishtank"
+
+DELIMITER $$
+CREATE OR REPLACE FUNCTION Encode_password(pass VARCHAR(63)) RETURNS VARCHAR(63)
+BEGIN
+	DECLARE encoded_pass VARCHAR(63);
+    SET encoded_pass = CONCAT(pass, @seed);
+    RETURN encoded_pass;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE OR REPLACE FUNCTION Decode_password(pass VARCHAR(63)) RETURNS VARCHAR(63)
+BEGIN
+    DECLARE decoded_pass VARCHAR(63);
+    SET   decoded_pass = IF(pass LIKE "%fishtank", REPLACE(pass, @seed, ''), pass);
+    RETURN decoded_pass;
+    
+END $$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE Add_user(
+    loginN VARCHAR(63),
+    mail VARCHAR(63),
+    pass VARCHAR(63)
+)
+BEGIN
+	SET @p0=`Encode_password`(pass);
+	INSERT INTO users (username, mail, pass) VALUES(
+        loginN,
+        mail,
+        @p0
+	);
+END $$
+DELIMITER ;
