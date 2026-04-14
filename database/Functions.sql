7c143c87f5340001e0fca1337d0e7c92cd08c7bd
planetixin <gax36003@student.gdynia.merito.pl>
2026-04-14 20:09:16 +0200

    logowanie_database update2
    dodano funckję check_login_and_pass która sprawdza chasło i login

--- database/Functions.sql
+++ database/Functions.sql
@@ -6,7 +6,7 @@ DELIMITER $$
 CREATE OR REPLACE FUNCTION Encode_password(pass VARCHAR(63)) RETURNS VARCHAR(63)
 BEGIN
 	DECLARE encoded_pass VARCHAR(63);
-    SET encoded_pass = CONCAT(pass, @seed);
+    SET encoded_pass = CONCAT(pass, "fishtank");
     RETURN encoded_pass;
 END$$
 DELIMITER ;
@@ -15,7 +15,7 @@ DELIMITER $$
 CREATE OR REPLACE FUNCTION Decode_password(pass VARCHAR(63)) RETURNS VARCHAR(63)
 BEGIN
     DECLARE decoded_pass VARCHAR(63);
-    SET   decoded_pass = IF(pass LIKE "%fishtank", REPLACE(pass, @seed, ''), pass);
+    SET   decoded_pass = IF(pass LIKE "%fishtank", REPLACE(pass, "fishtank", ''), pass);
     RETURN decoded_pass;
     
 END $$
@@ -35,5 +35,21 @@ BEGIN
         mail,
         @p0
 	);
+END $$
+
+
+DELIMITER $$
+CREATE OR REPLACE FUNCTION Check_login_and_pass(loginN VARCHAR(63), pass VARCHAR(63)) RETURNS VARCHAR(63)
+BEGIN
+    DECLARE user0 VARCHAR(63); 
+    SELECT `Decode_password`(users.pass) INTO user0 FROM users WHERE loginN = users.userName;
+
+    Set @isLogin := ISNULL(user0);
+    IF user0 = pass
+    THEN
+        return "haslo i login są poprawne";
+    ELSE
+	    return "haslo lub login są niepoprawne";
+    END IF;
 END $$
 DELIMITER ;
